f3335cdb1318f04f74ea9ca745c72484f70709d6
Liad Pasker <liadpasker@Liads-MacBook-Pro.local>
2020-04-13 17:24:33 +0300

    logs added

--- app-server/mr_server.js
+++ app-server/mr_server.js
@@ -19,7 +19,7 @@ var express = require('express'),
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.status(404).send({url: req.originalUrl + ' not found'})
+    //res.status(404).send({url: req.originalUrl + ' not found'})
     next();
   });
   
